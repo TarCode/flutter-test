14fcb1ba072d0e4517970110051f4c4c2ad60ebd
TarCode <tarcode33@gmail.com>
2019-02-05 22:45:10 +0200

    created initial flutter app with stateful widget

---
+++ lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:english_words/english_words.dart';
+
+void main() => runApp(MyApp());
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return MaterialApp(
+      title: 'Welcome to Blocktame',
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Welcome to Blocktame')
+        ),
+        body: Center(
+          child: RandomWords(),
+        )
+      )
+    );
+  }
+}
+
+class RandomWords extends StatefulWidget {
+  @override
+  RandomWordsState createState() => new RandomWordsState();
+}
+
+class RandomWordsState extends State<RandomWords> {
+  @override
+  Widget build(BuildContext context) {
+    final wordPair = WordPair.random();
+    return Text(wordPair.asPascalCase);
+  }
+}
