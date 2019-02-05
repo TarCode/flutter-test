d511ee797f47d1473e7188a1a14705805e93a9b9
TarCode <tarcode33@gmail.com>
2019-02-05 23:03:15 +0200

    added _saved Set variable, add trailing icon to _buildRow widget, create alreadySaved bool variable to check if  _saved contains word and render appropriate icon

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,6 @@ import 'package:english_words/english_words.dart';
 
 void main() => runApp(MyApp());
 
-
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -29,6 +28,7 @@ class RandomWords extends StatefulWidget {
 
 class RandomWordsState extends State<RandomWords> {
   final _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = new Set<WordPair>();
   final _biggerFont = const TextStyle(fontSize: 18.0);
   @override
 
@@ -57,12 +57,16 @@ class RandomWordsState extends State<RandomWords> {
   }
 
   Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
     return ListTile(
       title: Text(
         pair.asPascalCase,
         style: _biggerFont,
       ),
+        trailing: new Icon(   // Add the lines from here... 
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),   
     );
   }
-
 }
